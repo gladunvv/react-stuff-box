2c5138357abaa07b91dd9c1eca77f99bcbe7197f
vladislav <bincha.1997@gmail.com>
2020-07-16 23:14:18 +0900

    feat: added logic

--- src/components/CalculatorComponents/CalculatorKey/CalculatorKey.js
+++ src/components/CalculatorComponents/CalculatorKey/CalculatorKey.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import cn from 'classnames';
 
 import './CalculatorKey.scss';
 
-export const CalculatorKey = ({ symb, typeKey }) => {
-  const classKey = cn('key-calculate', typeKey);
-  return (
-    <>
-      <div className={classKey}>{symb}</div>
-    </>
-  );
-};
+export class CalculatorKey extends Component {
+  handleClick = () => {
+    this.props.clickHandler(this.props.symb);
+  };
+  render() {
+    const { symb, typeKey } = this.props;
+    const classKey = cn('key-calculate', typeKey);
+    return (
+      <>
+        <div onClick={this.handleClick} className={classKey}>
+          {symb}
+        </div>
+      </>
+    );
+  }
+}
--- src/page/CalculatorPage/CalculatorPage.js
+++ src/page/CalculatorPage/CalculatorPage.js
@@ -1,45 +1,132 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { CalculatorKey } from '../../components/CalculatorComponents/CalculatorKey';
 import { CalculatorDisplay } from '../../components/CalculatorComponents/CalculatorDisplay';
+import { calculate } from './CalculatorLogic/calculate';
 
 import './CalculatorPage.scss';
 
-export const CalculatorPage = () => {
-  return (
-    <>
-      <div className='calculator'>
-        <div className='calculate-wrapper'>
-          <CalculatorDisplay />
-          <div className='calculate-keys'>
-            <div className='calculate-left-block'>
-              <div className='calculate-functions'>
-                <CalculatorKey typeKey='key-numbers' symb='AC' />
-                <CalculatorKey typeKey='key-numbers' symb='±' />
-                <CalculatorKey typeKey='key-numbers' symb='%' />
+export class CalculatorPage extends Component {
+  state = {
+    total: null,
+    next: null,
+    operation: null,
+  };
+
+  handleClick = (buttonName) => {
+    console.log('buttonName :>> ', buttonName);
+    console.log('object :>> ', this.state.total);
+    this.setState(calculate(this.state, buttonName));
+  };
+
+  render() {
+    return (
+      <>
+        <div className='calculator'>
+          <div className='calculate-wrapper'>
+            <CalculatorDisplay />
+            <div className='calculate-keys'>
+              <div className='calculate-left-block'>
+                <div className='calculate-functions'>
+                  <CalculatorKey
+                    clickHandler={this.handleClick}
+                    typeKey='key-numbers'
+                    symb='AC'
+                  />
+                  <CalculatorKey
+                    clickHandler={this.handleClick}
+                    typeKey='key-numbers'
+                    symb='±'
+                  />
+                  <CalculatorKey
+                    clickHandler={this.handleClick}
+                    typeKey='key-numbers'
+                    symb='%'
+                  />
+                </div>
+                <div className='calculate-numbers'>
+                  <CalculatorKey
+                    clickHandler={this.handleClick}
+                    typeKey='key-numbers'
+                    symb='1'
+                  />
+                  <CalculatorKey
+                    clickHandler={this.handleClick}
+                    typeKey='key-numbers'
+                    symb='2'
+                  />
+                  <CalculatorKey
+                    clickHandler={this.handleClick}
+                    typeKey='key-numbers'
+                    symb='3'
+                  />
+                  <CalculatorKey
+                    clickHandler={this.handleClick}
+                    typeKey='key-numbers'
+                    symb='4'
+                  />
+                  <CalculatorKey
+                    clickHandler={this.handleClick}
+                    typeKey='key-numbers'
+                    symb='5'
+                  />
+                  <CalculatorKey
+                    clickHandler={this.handleClick}
+                    typeKey='key-numbers'
+                    symb='6'
+                  />
+                  <CalculatorKey
+                    clickHandler={this.handleClick}
+                    typeKey='key-numbers'
+                    symb='7'
+                  />
+                  <CalculatorKey
+                    clickHandler={this.handleClick}
+                    typeKey='key-numbers'
+                    symb='8'
+                  />
+                  <CalculatorKey
+                    clickHandler={this.handleClick}
+                    typeKey='key-numbers'
+                    symb='9'
+                  />
+                  <CalculatorKey
+                    clickHandler={this.handleClick}
+                    typeKey='key-numbers'
+                    symb='0'
+                  />
+                </div>
               </div>
-              <div className='calculate-numbers'>
-                <CalculatorKey typeKey='key-numbers' symb='1' />
-                <CalculatorKey typeKey='key-numbers' symb='2' />
-                <CalculatorKey typeKey='key-numbers' symb='3' />
-                <CalculatorKey typeKey='key-numbers' symb='4' />
-                <CalculatorKey typeKey='key-numbers' symb='5' />
-                <CalculatorKey typeKey='key-numbers' symb='6' />
-                <CalculatorKey typeKey='key-numbers' symb='7' />
-                <CalculatorKey typeKey='key-numbers' symb='8' />
-                <CalculatorKey typeKey='key-numbers' symb='9' />
-                <CalculatorKey typeKey='key-numbers' symb='0' />
+              <div className='calculate-operations'>
+                <CalculatorKey
+                  clickHandler={this.handleClick}
+                  typeKey='key-operations'
+                  symb='÷'
+                />
+                <CalculatorKey
+                  clickHandler={this.handleClick}
+                  typeKey='key-operations'
+                  symb='×'
+                />
+                <CalculatorKey
+                  clickHandler={this.handleClick}
+                  typeKey='key-operations'
+                  symb='-'
+                />
+                <CalculatorKey
+                  clickHandler={this.handleClick}
+                  typeKey='key-operations'
+                  symb='+'
+                />
+                <CalculatorKey
+                  clickHandler={this.handleClick}
+                  typeKey='key-operations'
+                  symb='='
+                />
               </div>
-            </div>
-            <div className='calculate-operations'>
-              <CalculatorKey typeKey='key-operations' symb='÷' />
-              <CalculatorKey typeKey='key-operations' symb='×' />
-              <CalculatorKey typeKey='key-operations' symb='−' />
-              <CalculatorKey typeKey='key-operations' symb='+' />
-              <CalculatorKey typeKey='key-operations' symb='=' />
             </div>
           </div>
         </div>
-      </div>
-    </>
-  );
-};
+      </>
+    );
+  }
+}
