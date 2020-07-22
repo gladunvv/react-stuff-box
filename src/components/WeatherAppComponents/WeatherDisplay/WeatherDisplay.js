6f28821c2c5da65b41973d08b5d2e72ec724ddf0
vladislav <bincha.1997@gmail.com>
2020-07-22 23:52:21 +0900

    feat: update weather page

---
+++ src/components/WeatherAppComponents/WeatherDisplay/WeatherDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const WeatherDisplay = () => {
+  return (
+    <>
+      <h1>WeatherDisplay</h1>
+    </>
+  );
+};
--- src/page/WeatherAppPage/WeatherAppPage.js
+++ src/page/WeatherAppPage/WeatherAppPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { WeatherDisplay } from '../../components/WeatherAppComponents/WeatherDisplay';
 
 export const WeatherAppPage = () => {
   return (
     <>
       <h1>WeatherApp</h1>
+      <WeatherDisplay />
     </>
   );
 };
