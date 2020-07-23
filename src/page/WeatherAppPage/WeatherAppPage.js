7c9cafdb1ecb13706d552ac58bbbc2d862c463d6
vladislav <bincha.1997@gmail.com>
2020-07-23 23:41:56 +0900

    feat: update weather page

--- src/page/WeatherAppPage/WeatherAppPage.js
+++ src/page/WeatherAppPage/WeatherAppPage.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { WeatherDisplay } from '../../components/WeatherAppComponents/WeatherDisplay';
 
 import './WeatherAppPage.scss';
+const PLACES = [
+  { name: 'Moscow', zip: '524894' },
+  { name: 'Blagoveshchensk', zip: '576116' },
+  { name: 'Svobodnyy', zip: '485449' },
+  { name: 'Khabarovsk', zip: '2022890' },
+];
 
 export const WeatherAppPage = () => {
   return (
     <>
       <h1>WeatherApp</h1>
-      <WeatherDisplay />
+      <WeatherDisplay key={1} zip={PLACES[1].zip} />
     </>
   );
 };
