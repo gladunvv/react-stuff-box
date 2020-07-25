eccfe29c2bad82eb44cffdbb51600a3bc4f359a1
vladislav <bincha.1997@gmail.com>
2020-07-25 23:35:28 +0900

    Updates

---
+++ precache-manifest.91719e3efa8a117c85b0f67a6e5a167b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04e918ae9b9a06f39b3e383dc35d9be5",
+    "url": "/react-stuff-box/index.html"
+  },
+  {
+    "revision": "9fafe9e43531f7be9e53",
+    "url": "/react-stuff-box/static/css/main.13b2cf8b.chunk.css"
+  },
+  {
+    "revision": "4376f268e70ebe356ddd",
+    "url": "/react-stuff-box/static/js/2.453fb9f4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-stuff-box/static/js/2.453fb9f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fafe9e43531f7be9e53",
+    "url": "/react-stuff-box/static/js/main.ddf14cea.chunk.js"
+  },
+  {
+    "revision": "8a4e8a24a82470e24dc7",
+    "url": "/react-stuff-box/static/js/runtime-main.4ecce763.js"
+  }
+]);
