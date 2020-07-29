da10c411d53b727957c6ba9f6144b51b296526f9
vladislav <bincha.1997@gmail.com>
2020-07-29 23:54:09 +0900

    feat: update count timer

---
+++ src/components/CountTimerComoponents/CountTimerClockInput/CountTimerClockInput.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './CountTimerClockInput.scss';
+
+export const CountTimerClockInput = () => {
+  return (
+    <>
+      <div className='count-timer-input'>
+        <input
+          type='date'
+          name='time-to'
+          className='time-to'
+          id='time-to'
+          value=''
+          onchange='calcTime(this.value)'
+        />
+      </div>
+    </>
+  );
+};
--- src/page/CountTimerPage/CountTImerPage.js
+++ src/page/CountTimerPage/CountTImerPage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import { CountTimerClockInput } from '../../components/CountTimerComoponents/CountTimerClockInput';
+
 export const CountTimerPage = () => {
   return (
     <>
       <h1>Countdown timer page</h1>
+      <CountTimerClockInput />
     </>
   );
 };
