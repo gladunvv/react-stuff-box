e902f3aa9b7bc98052b7a35eeae52849c9583f90
vladislav <bincha.1997@gmail.com>
2020-08-01 23:42:21 +0900

    feat: update timer page

--- src/components/CountTimerComoponents/CountTimerDisplay/CountTimerDisplay.js
+++ src/components/CountTimerComoponents/CountTimerDisplay/CountTimerDisplay.js
@@ -2,27 +2,27 @@ import React from 'react';
 
 import './CountTimerDisplay.scss';
 
-export const CountTimerDisplay = () => {
+export const CountTimerDisplay = ({ timeDown }) => {
   return (
     <>
       <div className='count-timer-display'>
         <div className='clock-column'>
-          <p className='clock-timer'>2</p>
+          <p className='clock-timer'>{timeDown.days}</p>
           <p className='clock-label'>Days</p>
         </div>
 
         <div className='clock-column'>
-          <p className='clock-timer'>12</p>
+          <p className='clock-timer'>{timeDown.hours}</p>
           <p className='clock-label'>Hours</p>
         </div>
 
         <div className='clock-column'>
-          <p className='clock-timer'>03</p>
+          <p className='clock-timer'>{timeDown.minutes}</p>
           <p className='clock-label'>Minutes</p>
         </div>
 
         <div className='clock-column'>
-          <p className='clock-timer'>12</p>
+          <p className='clock-timer'>{timeDown.seconds}</p>
           <p className='clock-label'>Seconds</p>
         </div>
       </div>
--- src/page/CountTimerPage/CountTImerPage.js
+++ src/page/CountTimerPage/CountTImerPage.js
@@ -5,28 +5,48 @@ import { CountTimerDisplay } from '../../components/CountTimerComoponents/CountT
 import './CountTimerPage.scss';
 
 export class CountTimerPage extends Component {
-  countDown = (date) => {
-    let toDate = new Date(date).getTime();
-    let now = new Date().getTime();
-    let distance = toDate - now;
-
-    console.log('distance :>> ', now);
-
-    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor(
-      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
-    );
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    console.log('days :>> ', days);
+  state = {
+    timer: {
+      days: 'T',
+      hours: 'I',
+      minutes: 'M',
+      seconds: 'E',
+    },
   };
+
+  useInterval = (date) => {
+    const intervalStart = setInterval(() => {
+      let toDate = new Date(date).getTime();
+      let now = new Date().getTime();
+      let distance = toDate - now;
+
+      let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      let hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+      );
+      let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      if (distance < 0) {
+        clearInterval(this.useInterval);
+      } else {
+        this.setState({ timer: { days, hours, minutes, seconds } });
+      }
+      console.log('time :>> ', seconds);
+    }, 1000);
+  };
+
+  countDown = (date) => {
+    // this.useInterval(date);
+  };
+
   render() {
+    const time = this.state.timer;
     return (
       <>
         <div className='count-timer'>
           <h1>Countdown timer page</h1>
           <CountTimerClockInput startTimer={this.countDown} />
-          <CountTimerDisplay />
+          <CountTimerDisplay timeDown={time} />
         </div>
       </>
     );
